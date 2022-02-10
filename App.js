0d8d2efbc9f994cbdd2edb505198223e08615927
Tempus-Dominus <bioerror@hotmail.com>
2022-02-10 16:50:52 -0700

    Added Card Class; Touched up Landing Page

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { Component } from 'react';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -27,6 +27,46 @@ const MyStack = () => {
         )
 }
 
+class Card extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            faceup: false,
+            cardImage: null,
+            cardNumber: props.cardNumber,
+        }
+    }
+
+    //Onpress event handler for this card
+    flipCard = () => {
+        this.setState({
+            faceup: !this.state.faceup,
+        });
+    }
+
+    render() {
+        //conditionally decide what to show on the card
+        let showSide;
+        let cardNumber;
+        if (this.state.faceup) {
+            showSide=styles.cardFace;
+            cardNumber = this.state.cardNumber;
+        } else {
+            showSide=styles.cardBack;
+            cardNumber="";
+        }
+
+        return(
+            <TouchableOpacity
+                style={showSide}
+                onPress={this.flipCard}
+            >
+                <Text>Card {cardNumber}</Text>
+            </TouchableOpacity>
+        );
+    }
+}
+
 export default class App extends Component {
     constructor() {
         var today = new Date(),
@@ -46,8 +86,8 @@ export default class App extends Component {
         return (
             <View style={styles.container}>
                 <View style={{alignItems: 'center',}}>
-                    <Text style={{fontWeight: 'bold', fontSize: 25, padding: 10}}>Welcome to MadLibs</Text>
-                    <Text style={{paddingBottom: 40}}>Please enter a Name, Noun, and an Event. Once done, press 'Make my hall pass' to generate your hall pass.</Text>
+                    <Text style={{fontWeight: 'bold', fontSize: 25, padding: 10}}>Welcome to Card Match</Text>
+                    <Text style={{paddingBottom: 40, paddingLeft: 15, paddingRight: 15,}}>This game is played by selecting 1 of 16 cards and trying to find it's matching pair. Try to do this in the shortest time.</Text>
                 </View>
                 
                 <View style={styles.buttonContainer}>
@@ -68,33 +108,12 @@ export default class App extends Component {
         )
     }
 
-    ProfileScreen = ({ navigation, route }) => {
+    GameScreen = ({ navigation, route }) => {
 
         return (
-            <View style={styles.hallPassContainer}>
-                <View style={styles.hallPassVertical}>
-                    <Text style={styles.hallPassVerticalText}> Hall Pass</Text>
-                </View>
-                <View style={styles.hallPassHorizontal}>
-                    <View style={styles.libTitle}>
-                        <Text style={styles.libTitleText}>Mad Libs</Text>
-                    </View>
-                    <View style={styles.libBody}>
-                        <Text style={styles.libDate}> Date: <Text style={[{textDecorationLine: 'underline',}]}>{this.state.currentDate}</Text></Text>
-                        <Text style={styles.libText}><View style={styles.underline}><Text style={styles.fillBlanks}>{route.params.theName}</Text></View> is too cool</Text>
-                        <Text style={[{fontSize: 10, marginLeft: 60,}]}>NAME</Text>
-                        <Text style={styles.libText}>for <View style={styles.underline}><Text style={styles.fillBlanks}>{route.params.theNoun}</Text></View> class.</Text>
-                        <Text style={[{fontSize: 10, marginLeft: 90,}]}>NOUN</Text>
-                        <Text style={styles.libText}>Instead, he/she will be</Text>
-                        <Text style={styles.libText}>attending the <View style={styles.underline}><Text style={styles.fillBlanks}>{route.params.theEvent}</Text></View></Text>
-                        <Text style={[{fontSize: 10, marginLeft: 180,}]}>EVENT</Text>
-                    </View>
-                    <View style={styles.libSig}>
-                        <View style={styles.libSigBox}>
-                            <Text style={styles.libSigTitle}>Signature</Text>
-                        </View>
-                    </View>
-                </View>
+            <View>
+                
+                
                 <StatusBar style="auto" />
             </View>
         )
@@ -112,9 +131,9 @@ export default class App extends Component {
                     />
 
                     <Stack.Screen
-                        name="Hallpass"
-                        component={this.ProfileScreen}
-                        options={{ title: 'Hallpass' }}
+                        name="Card Match"
+                        component={this.GameScreen}
+                        options={{ title: 'Card Match' }}
                     />
                 </Stack.Navigator>
             </NavigationContainer>
@@ -134,103 +153,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     },
-  textInput: {
-    height: 40,
-    width: 300,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+  cardBack: {
+    backgroundColor: "aqua",
+    width: 100,
+    height: 100,
+    margin: 10,
+    padding: 20,
+    borderRadius: 10,
   },
-  hallPassVertical: {
-    flex: 0,
-    width: '30%',
-    height: '100%',
-    //borderWidth: 1,
-    flexWrap: 'wrap',
-    backgroundColor: '#fff',
-    justifyContent: 'flex-end',
-    },
-  hallPassHorizontal: {
-    flex: 1,
-    flexDirection: 'column',
-    //borderWidth: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'flex-end',
-    },
-  libTitle: {
-    width: '100%',
-    height: '10%',
+  cardFace: {
+    backgroundColor: "red",
+    width: 100,
+    height: 100,
+    margin: 10,
+    padding: 20,
+    borderRadius: 10,
   },
-  libTitleText: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    justifyContent: 'center',
-    textAlign: 'center',
-    //borderWidth: 1,
+  cardContainer: {
+    marginTop: 10,
   },
-  libText: {
-    fontSize: 40,
-    fontWeight: 'bold',
-    justifyContent: 'center',
-    textAlign: 'center',
-    //borderWidth: 1,
-  },
-  libBody: {
-    width: '100%',
-    height: '70%',
-    //borderWidth: 1,
-  },
-  libSig: {
-    width: '100%',
-    height: '20%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    //borderWidth: 1,
-  },
-  libSigBox: {
-    width: '85%',
-    height: '65%',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    borderWidth: 4,
-  },
-  libSigTitle: {
-    fontSize: 10,
-    padding: 2.5,
-    textAlign: 'center',
-    fontFamily: 'monospace',
-    fontWeight: 'bold',
-  },
-  libDate: {
-    fontSize: 20,
-    paddingTop: 20,
-  },
-  libText: {
-    fontSize: 20,
-    paddingTop: 35,
-    paddingLeft: 20,
-  },
-  hallPassVerticalText: {
-      transform: [{rotate: '-90deg'}],
-      fontSize: 85,
-      fontWeight: 'bold',
-  },
-  hallPassContainer: {
-      flex: 1,
-      borderWidth: 1,
-      flexDirection: 'row',
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-  },
-  underline: {
-      borderBottomWidth: 1,
-      borderBottomColor: 'black',
-      width: 100,
-  },
-  fillBlanks: {
-    textAlign: 'center',
-    fontFamily: 'monospace',
-    fontWeight: 'bold',
+  cardRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
